d7f2773f511632d0386f686bbb39f273451a4946
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-09-12 10:39:46 +0100

    Update solved troubleshoot_09.py
    no need for else clause

--- troubleshoot_09.py
+++ troubleshoot_09.py
@@ -9,10 +9,9 @@ Author: Nathan Lutala (nlutala)
 def calculate_area(radius):
     if radius < 0:
         raise ValueError("Negative radius given.")
-    else:
-        pi = 3.14
-        area = pi * radius ** 2
-        return area
+    pi = 3.14
+    area = pi * radius ** 2
+    return area
 
 if __name__ == "__main__":
     print("Area of circle:", calculate_area(5))
