6d4ea7bcaa4f78f199846eed1f8858042e12253b
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-09-12 09:48:54 +0100

    Solve troubleshoot_5.py

--- troubleshoot_5.py
+++ troubleshoot_5.py
@@ -1,6 +1,20 @@
+"""
+troubleshoot_5.py
+
+Troubleshoot the issue 5: (created by Generative AI)
+
+Author: Nathan Lutala (nlutala)
+"""
+
 def get_element_at_index(lst, index):
-    return lst[index]
+    try:
+        return lst[index]
+    except IndexError:
+        return None
 
 if __name__ == "__main__":
     my_list = [10, 20, 30, 40, 50]
     print(get_element_at_index(my_list, 5))
+    # my_list only has indicies in the range of 0-4.
+    # We need to handle this exception, preferably in the get_element_at_index function
+    # (we could return None)?
