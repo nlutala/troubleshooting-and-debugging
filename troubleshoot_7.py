e612cd9b2d1acc5903a8441b3902154ffe542c4b
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-09-12 10:12:00 +0100

    Update troubleshoot_7.py

--- troubleshoot_7.py
+++ troubleshoot_7.py
@@ -1,3 +1,13 @@
+"""
+troubleshoot_7.py
+
+Troubleshoot the issue 7: (created by Generative AI)
+
+Author: Nathan Lutala (nlutala)
+
+Nothing to debug here, the code is correct
+"""
+
 import numpy as np
 
 def matrix_multiplication(A, B):
@@ -8,6 +18,7 @@ def matrix_multiplication(A, B):
                 result[i][j] += A[i][k] * B[k][j]
     return result
 
-A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-B = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
-print(matrix_multiplication(A, B))
+if __name__ == "__main__":
+    A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    B = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
+    print(matrix_multiplication(A, B))
