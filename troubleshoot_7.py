fea6757999e63f66e434fcb3a9464d4f21085392
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-09-12 09:57:00 +0100

    Create troubleshoot_7.py

---
+++ troubleshoot_7.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def matrix_multiplication(A, B):
+    result = np.zeros((len(A), len(B[0])))
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                result[i][j] += A[i][k] * B[k][j]
+    return result
+
+A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+B = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
+print(matrix_multiplication(A, B))
