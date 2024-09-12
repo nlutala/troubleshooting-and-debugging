ca49c48d03986aa485dacf0679df74b7a903e4b4
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-09-12 09:29:24 +0100

    Create troubleshoot_2.py

---
+++ troubleshoot_2.py
@@ -0,0 +1,21 @@
+"""
+troubleshoot_2.py
+
+Troubleshoot the issue 2: (Intermediate - created by Generative AI)
+
+Author: Nathan Lutala (nlutala)
+"""
+
+def find_max(numbers):
+    max_num = numbers[0]
+    for num in numbers:
+        # The logic error lies here.
+        # Num needs to be bigger than max_num for max_num to be updated to num
+        # if num < max_num:
+        if num > max_num:
+            max_num = num
+    return max_num
+
+if __name__ == "__main__":
+    numbers = [3, 5, 2, 8, 1]
+    print("The maximum number is:", find_max(numbers))
