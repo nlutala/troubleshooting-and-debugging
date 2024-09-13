617dee14a02d3bb85cf7ad097db263de9c53fb8b
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-09-13 10:24:45 +0100

    Create troubleshoot_15.py

---
+++ solutions/troubleshoot_15.py
@@ -0,0 +1,13 @@
+"""
+problems/troubleshoot_15.py
+
+Troubleshoot the issue 15: (created by Generative AI)
+
+Author: Nathan Lutala (nlutala)
+"""
+
+if __name__ == "__main__":
+    x = 10
+    # if x = 10: - x is being assigned the value 10, but not compared.
+    if x == 10:
+        print("x is 10")
