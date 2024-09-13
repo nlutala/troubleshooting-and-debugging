f43bf39f1c73c4c7c3e1ede1aca8df930e2e6894
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-09-13 10:22:15 +0100

    Create troubleshoot_13.py

---
+++ solutions/troubleshoot_13.py
@@ -0,0 +1,13 @@
+"""
+solutions/troubleshoot_13.py
+
+Troubleshoot the issue 13: (created by Generative AI)
+
+Author: Nathan Lutala (nlutala)
+"""
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    # print(my_list[5]) # 5 is not a valid index. To get the last element, you can do
+    # the following
+    print(my_list[-1])
