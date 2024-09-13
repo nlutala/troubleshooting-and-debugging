e07e2015c6a5565603f42732ba098dc47d89044f
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-09-13 10:20:11 +0100

    Add troubleshoot_12.py

---
+++ solutions/troubleshoot_12.py
@@ -0,0 +1,12 @@
+"""
+solutions/troubleshoot_12.py
+
+Troubleshoot the issue 12: (created by Generative AI)
+
+Author: Nathan Lutala (nlutala)
+"""
+
+if __name__ == "__main__":
+    # for i in range(5) # A colon was missing
+    for i in range(5):
+        print(i)
