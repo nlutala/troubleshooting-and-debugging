7a508a714b1e649732fae21b1fa072632c6d6331
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-09-13 10:18:34 +0100

    Add troubleshoot_11.py

---
+++ solutions/troubleshoot_11.py
@@ -0,0 +1,16 @@
+"""
+solutions/troubleshoot_11.py
+
+Troubleshoot the issue 11: (created by Generative AI)
+
+Author: Nathan Lutala (nlutala)
+"""
+
+
+def add_numbers(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    # print(add_numbers(5))  # Add numbers accepts 2 parameters, but only one was given
+    print(add_numbers(5, 1))
