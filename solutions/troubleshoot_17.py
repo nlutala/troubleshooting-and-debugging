19a471a2e38249cfeff3cc830f0363b10d535b55
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-09-13 10:29:58 +0100

    Create troubleshoot_17.py

---
+++ solutions/troubleshoot_17.py
@@ -0,0 +1,13 @@
+"""
+solutions/troubleshoot_17.py
+
+Troubleshoot the issue 17: (created by Generative AI)
+
+Author: Nathan Lutala (nlutala)
+"""
+
+import math
+
+if __name__ == "__main__":
+    # print(math.sqrt(-1)) - You can't square root -1
+    print(math.sqrt(-1 * -1)) # I just did this to make a positive number (1)
